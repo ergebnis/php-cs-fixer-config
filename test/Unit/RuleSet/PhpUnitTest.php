4f475e0d83cd93d67fa145304f26885b57afde4b
Andreas Möller <am@localheinz.com>
2020-12-19 08:26:26 +0100

    Enhancement: Enable and configure php_unit_test_case_static_method_calls fixer

--- test/Unit/RuleSet/PhpUnitTest.php
+++ test/Unit/RuleSet/PhpUnitTest.php
@@ -265,7 +265,9 @@ final class PhpUnitTest extends AbstractRuleSetTestCase
         'php_unit_size_class' => false,
         'php_unit_strict' => false,
         'php_unit_test_annotation' => false,
-        'php_unit_test_case_static_method_calls' => false,
+        'php_unit_test_case_static_method_calls' => [
+            'call_type' => 'this',
+        ],
         'php_unit_test_class_requires_covers' => false,
         'phpdoc_add_missing_param_annotation' => false,
         'phpdoc_align' => true,

148432cc05716b3e3b9abf9811cefc40dfb7da6f
Andreas Möller <am@localheinz.com>
2022-08-19 10:38:56 +0200

    Fix: Use method

--- test/Unit/FactoryTest.php
+++ test/Unit/FactoryTest.php
@@ -31,7 +31,7 @@ final class FactoryTest extends Framework\TestCase
         $targetPhpVersion = \PHP_VERSION_ID + 1;
 
         $ruleSet = new Test\Double\Config\RuleSet\DummyRuleSet(
-            self::faker()->word,
+            self::faker()->word(),
             [],
             $targetPhpVersion,
         );
@@ -59,7 +59,7 @@ final class FactoryTest extends Framework\TestCase
         ];
 
         $ruleSet = new Test\Double\Config\RuleSet\DummyRuleSet(
-            self::faker()->word,
+            self::faker()->word(),
             $rules,
             $targetPhpVersion,
         );
@@ -98,7 +98,7 @@ final class FactoryTest extends Framework\TestCase
         ];
 
         $ruleSet = new Test\Double\Config\RuleSet\DummyRuleSet(
-            self::faker()->word,
+            self::faker()->word(),
             $rules,
             \PHP_VERSION_ID,
         );

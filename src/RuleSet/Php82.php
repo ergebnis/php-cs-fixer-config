b1513abb57a12301861c632725caa99b6f3a918d
Andreas Möller <am@localheinz.com>
2023-04-27 18:56:34 +0200

    Fix: Disable php_unit_test_class_requires_covers fixer

--- src/RuleSet/Php82.php
+++ src/RuleSet/Php82.php
@@ -480,7 +480,7 @@ final class Php82 extends AbstractRuleSet implements ExplicitRuleSet
             'call_type' => 'self',
             'methods' => [],
         ],
-        'php_unit_test_class_requires_covers' => true,
+        'php_unit_test_class_requires_covers' => false,
         'phpdoc_add_missing_param_annotation' => [
             'only_untyped' => true,
         ],

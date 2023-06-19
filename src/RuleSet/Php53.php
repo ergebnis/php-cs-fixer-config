8b452e99a371aa9715c383ed2298e6aee8d9d9d5
Andreas Möller <am@localheinz.com>
2023-06-19 09:46:38 +0200

    Fix: Configure blank_lines_before_namespace instead of deprecated single_blank_line_before_namespace fixer

--- src/RuleSet/Php53.php
+++ src/RuleSet/Php53.php
@@ -59,7 +59,10 @@ final class Php53 extends AbstractRuleSet implements ExplicitRuleSet
             ],
         ],
         'blank_line_between_import_groups' => false,
-        'blank_lines_before_namespace' => false,
+        'blank_lines_before_namespace' => [
+            'max_line_breaks' => 2,
+            'min_line_breaks' => 2,
+        ],
         'cast_spaces' => [
             'space' => 'single',
         ],

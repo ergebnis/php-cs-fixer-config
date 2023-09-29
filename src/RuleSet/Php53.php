4d981d4a350a9323c92847a0a8a5df367349f8e0
Andreas Möller <am@localheinz.com>
2023-09-29 16:57:17 +0200

    Fix: Configure newly added instead of deprecated fixers

--- src/RuleSet/Php53.php
+++ src/RuleSet/Php53.php
@@ -92,6 +92,15 @@ final class Php53
                     'max_line_breaks' => 2,
                     'min_line_breaks' => 2,
                 ],
+                'braces_position' => [
+                    'allow_single_line_anonymous_functions' => false,
+                    'allow_single_line_empty_anonymous_classes' => false,
+                    'anonymous_classes_opening_brace' => 'same_line',
+                    'anonymous_functions_opening_brace' => 'same_line',
+                    'classes_opening_brace' => 'next_line_unless_newline_at_signature_end',
+                    'control_structures_opening_brace' => 'same_line',
+                    'functions_opening_brace' => 'next_line_unless_newline_at_signature_end',
+                ],
                 'cast_spaces' => [
                     'space' => 'single',
                 ],
@@ -117,7 +126,7 @@ final class Php53
                 'comment_to_phpdoc' => [
                     'ignored_tags' => [],
                 ],
-                'compact_nullable_typehint' => false,
+                'compact_nullable_type_declaration' => false,
                 'concat_space' => [
                     'spacing' => 'one',
                 ],
@@ -127,15 +136,6 @@ final class Php53
                 'control_structure_braces' => true,
                 'control_structure_continuation_position' => [
                     'position' => 'same_line',
-                ],
-                'curly_braces_position' => [
-                    'allow_single_line_anonymous_functions' => false,
-                    'allow_single_line_empty_anonymous_classes' => false,
-                    'anonymous_classes_opening_brace' => 'same_line',
-                    'anonymous_functions_opening_brace' => 'same_line',
-                    'classes_opening_brace' => 'next_line_unless_newline_at_signature_end',
-                    'control_structures_opening_brace' => 'same_line',
-                    'functions_opening_brace' => 'next_line_unless_newline_at_signature_end',
                 ],
                 'date_time_create_from_format_call' => true,
                 'date_time_immutable' => true,
@@ -301,7 +301,7 @@ final class Php53
                     'strict' => false,
                 ],
                 'native_function_type_declaration_casing' => true,
-                'new_with_braces' => [
+                'new_with_parentheses' => [
                     'anonymous_class' => true,
                     'named_class' => true,
                 ],
@@ -378,6 +378,9 @@ final class Php53
                 'no_trailing_whitespace' => true,
                 'no_trailing_whitespace_in_comment' => true,
                 'no_trailing_whitespace_in_string' => true,
+                'no_unneeded_braces' => [
+                    'namespaces' => false,
+                ],
                 'no_unneeded_control_parentheses' => [
                     'statements' => [
                         'break',
@@ -389,9 +392,6 @@ final class Php53
                         'return',
                         'switch_case',
                     ],
-                ],
-                'no_unneeded_curly_braces' => [
-                    'namespaces' => false,
                 ],
                 'no_unneeded_final_method' => [
                     'private_methods' => true,
--- test/Unit/RuleSet/Php56Test.php
+++ test/Unit/RuleSet/Php56Test.php
@@ -107,6 +107,15 @@ final class Php56Test extends ExplicitRuleSetTestCase
                 'max_line_breaks' => 2,
                 'min_line_breaks' => 2,
             ],
+            'braces_position' => [
+                'allow_single_line_anonymous_functions' => false,
+                'allow_single_line_empty_anonymous_classes' => false,
+                'anonymous_classes_opening_brace' => 'same_line',
+                'anonymous_functions_opening_brace' => 'same_line',
+                'classes_opening_brace' => 'next_line_unless_newline_at_signature_end',
+                'control_structures_opening_brace' => 'same_line',
+                'functions_opening_brace' => 'next_line_unless_newline_at_signature_end',
+            ],
             'cast_spaces' => [
                 'space' => 'single',
             ],
@@ -133,7 +142,7 @@ final class Php56Test extends ExplicitRuleSetTestCase
             'comment_to_phpdoc' => [
                 'ignored_tags' => [],
             ],
-            'compact_nullable_typehint' => false,
+            'compact_nullable_type_declaration' => false,
             'concat_space' => [
                 'spacing' => 'one',
             ],
@@ -143,15 +152,6 @@ final class Php56Test extends ExplicitRuleSetTestCase
             'control_structure_braces' => true,
             'control_structure_continuation_position' => [
                 'position' => 'same_line',
-            ],
-            'curly_braces_position' => [
-                'allow_single_line_anonymous_functions' => false,
-                'allow_single_line_empty_anonymous_classes' => false,
-                'anonymous_classes_opening_brace' => 'same_line',
-                'anonymous_functions_opening_brace' => 'same_line',
-                'classes_opening_brace' => 'next_line_unless_newline_at_signature_end',
-                'control_structures_opening_brace' => 'same_line',
-                'functions_opening_brace' => 'next_line_unless_newline_at_signature_end',
             ],
             'date_time_create_from_format_call' => true,
             'date_time_immutable' => true,
@@ -317,7 +317,7 @@ final class Php56Test extends ExplicitRuleSetTestCase
                 'strict' => false,
             ],
             'native_function_type_declaration_casing' => true,
-            'new_with_braces' => [
+            'new_with_parentheses' => [
                 'anonymous_class' => true,
                 'named_class' => true,
             ],
@@ -394,6 +394,9 @@ final class Php56Test extends ExplicitRuleSetTestCase
             'no_trailing_whitespace' => true,
             'no_trailing_whitespace_in_comment' => true,
             'no_trailing_whitespace_in_string' => true,
+            'no_unneeded_braces' => [
+                'namespaces' => false,
+            ],
             'no_unneeded_control_parentheses' => [
                 'statements' => [
                     'break',
@@ -407,9 +410,6 @@ final class Php56Test extends ExplicitRuleSetTestCase
                     'yield',
                     'yield_from',
                 ],
-            ],
-            'no_unneeded_curly_braces' => [
-                'namespaces' => false,
             ],
             'no_unneeded_final_method' => [
                 'private_methods' => true,

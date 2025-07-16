9b9043a6ed7f41d2a9219d35cbe2e2bf550044ec
Andreas Möller <am@localheinz.com>
2025-07-16 11:11:36 +0200

    Fix: Disable newly added fixer

--- src/RuleSet/Php73.php
+++ src/RuleSet/Php73.php
@@ -435,6 +435,7 @@ final class Php73
                 ],
                 'no_useless_else' => true,
                 'no_useless_nullsafe_operator' => false,
+                'no_useless_printf' => false,
                 'no_useless_return' => true,
                 'no_useless_sprintf' => true,
                 'no_whitespace_before_comma_in_array' => [
